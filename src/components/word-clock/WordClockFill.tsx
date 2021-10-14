377fd763bc7f8aa70f3623073944c127fc7a03a3
Simon Heys <si@simonheys.com>
2021-10-14 14:26:13 +0100

    Hide on scroll (#33)

--- src/components/word-clock/WordClockFill.tsx
+++ src/components/word-clock/WordClockFill.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { ChevronDown } from "../ui/icons";
 
+import AppearWhenInView from "../ui/AppearWhenInView";
 import WordClockEditable, { WordClockEditableProps } from "./WordClockEditable";
 import useWindowSize from "../../hooks/useWindowSize";
+import useWindowScroll from "../../hooks/useWindowScroll";
 
 import styles from "./WordClockFill.module.scss";
 
 const WordClockFill: React.FC<WordClockEditableProps> = (props) => {
   const windowSize = useWindowSize();
+  const windowScroll = useWindowScroll();
+
   const onChevronClick = React.useCallback(() => {
     if (typeof window !== "undefined") {
       const html = document.getElementsByTagName("html")[0];
@@ -16,6 +20,9 @@ const WordClockFill: React.FC<WordClockEditableProps> = (props) => {
       html.style.removeProperty("scroll-behavior");
     }
   }, [windowSize.innerHeight]);
+
+  const chevronVisible = windowScroll.scrollY < 16;
+
   return (
     <div className={styles.container}>
       <div className={"row h-100 mb-md-5"}>
@@ -24,9 +31,13 @@ const WordClockFill: React.FC<WordClockEditableProps> = (props) => {
         </div>
       </div>
       <div className={styles.chevronContainer}>
-        <div className={styles.chevron}>
-          <ChevronDown onClick={onChevronClick} />
-        </div>
+        <AppearWhenInView>
+          <div
+            className={chevronVisible ? styles.chevron : styles.chevronHidden}
+          >
+            <ChevronDown onClick={onChevronClick} />
+          </div>
+        </AppearWhenInView>
       </div>
     </div>
   );
