a2b8911c175580328a7edd6c0bad77a139efd840
Simon Heys <si@simonheys.com>
2022-02-06 17:09:45 +0000

    Remove generator Add assets

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import AppearWhenInView from "../ui/AppearWhenInView";
 import LinkA from "../ui/LinkA";
@@ -11,24 +9,6 @@ import LinkA from "../ui/LinkA";
 import styles from "./Blog.module.scss";
 import ImageResponsive from "../ui/ImageResponsive";
 import { getBlogDateFromPath, getPageForPath } from "../../modules/content";
-
-const CodeBlock = ({ node, inline, className, children, ...props }) => {
-  const match = /language-(\w+)/.exec(className || "");
-  return !inline && match ? (
-    <SyntaxHighlighter
-      style={dracula}
-      language={match[1]}
-      PreTag="div"
-      {...props}
-    >
-      {String(children).replace(/\n$/, "")}
-    </SyntaxHighlighter>
-  ) : (
-    <code className={className} {...props}>
-      {children}
-    </code>
-  );
-};
 
 const LinkComponent = ({ href, ...rest }: any) => {
   return <LinkA href={href} {...rest} />;
@@ -52,9 +32,17 @@ const Prose: React.FC<BlogProps> = ({ content }) => {
   const router = useRouter();
   const page = getPageForPath(router.asPath);
   const date = getBlogDateFromPath(router.asPath);
+  const age = new Date().getFullYear() - date.getFullYear();
+  const old = age >= 5;
   return (
     <AppearWhenInView>
       <div className={"container-fluid"}>
+        {old && (
+          <div className={"col-md-12"}>
+            This content is more than 5 years old and probably doesn't work any
+            more
+          </div>
+        )}
         <div className={"row mb-5"}>
           <div className={"col-md-6"}>
             <h1 className={styles.title}>
