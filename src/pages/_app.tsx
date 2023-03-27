47a36ad36a126d557d0e99e826eef80aefe3d457
Simon Heys <si@simonheys.com>
2023-03-27 18:40:07 +0100

    feat: remove ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,53 +1,16 @@
 import { Analytics } from '@vercel/analytics/react';
-import type {
-  AppProps,
-  /*, AppContext */
-} from 'next/app';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 
-import * as gtag from '../modules/gtag';
 import '../styles/globals.scss';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Script
-        id="gtag-js"
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-config"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Component {...pageProps} />
       <Analytics />
     </>
