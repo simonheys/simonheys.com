7e4f1afca2246fc72caae5c6c28496dd62ff2fc2
Simon Heys <si@simonheys.com>
2023-09-15 19:13:30 +0100

    feat: bun

--- src/utils/getImageColor.ts
+++ src/utils/getImageColor.ts
@@ -1,14 +1,25 @@
+import { Resvg } from '@resvg/resvg-js';
 import { colord } from 'colord';
+import fs from 'fs-extra';
 import { NdArray } from 'ndarray';
-import sharp from 'sharp';
 
 import getImagePixels from './getImagePixels';
 
 const getImageColor = async (filePath: string, type?: string) => {
   let pixels: NdArray<Uint8Array>;
   if (type === 'svg') {
-    const imageBuffer = await sharp(filePath).toFormat('png').toBuffer();
-    pixels = await getImagePixels(imageBuffer, 'image/png');
+    const svg = fs.readFileSync(filePath);
+    const resvg = new Resvg(svg, {
+      shapeRendering: 2,
+      textRendering: 2,
+      imageRendering: 0,
+      fitTo: {
+        mode: 'original',
+      },
+    });
+    const pngData = resvg.render();
+    const pngBuffer = pngData.asPng();
+    pixels = await getImagePixels(pngBuffer, 'image/png');
   } else {
     pixels = await getImagePixels(filePath);
   }
